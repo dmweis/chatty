e5b76c6f115690fdb2c5a73bd89ba65a9eea18a5
David Weis <davidmichaelweis@gmail.com>
2023-03-08 22:45:13 +0000

    Update talking cli

--- src/bin/audio_chat.rs
+++ src/bin/audio_chat.rs
@@ -1,17 +1,42 @@
 use anyhow::Context;
 use async_openai::{types::CreateTranscriptionRequestArgs, Client};
-use chatty::{chat_manager, configuration::AppConfig, mqtt::start_mqtt_service};
+use chatty::{
+    chat_manager::{self, generate_system_instructions},
+    configuration::AppConfig,
+    mqtt::start_mqtt_service,
+};
 use clap::Parser;
+use dialoguer::console::{Emoji, Term};
 use rumqttc::QoS;
 use std::io::BufRead;
+
+const ROBOT_EMOJI: Emoji = Emoji("🤖", "ChatGPT");
+const QUESTION_MARK_EMOJI: Emoji = Emoji("❓", "ChatGPT");
+
+const TRANSCRIBE_MODEL: &str = "whisper-1";
 
 #[derive(Parser, Debug)]
 #[command()]
 struct Cli {
+    /// disable streaming
+    #[arg(long)]
+    disable_streaming: bool,
+    /// do not save conversation
+    #[arg(long)]
+    no_save: bool,
+    /// save default config and exit
+    #[arg(long)]
+    create_config: bool,
+    /// copy token from local config to user config
+    #[arg(long)]
+    copy_local_config: bool,
+    /// Do not speak response
+    #[arg(short, long)]
+    mute: bool,
+
     /// The audio device to use
     #[arg(short, long)]
     device: Option<String>,
-
     /// Use the JACK host
     #[arg(short, long)]
     jack: bool,
@@ -20,37 +45,75 @@ struct Cli {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-    let config = AppConfig::load_dev_config()?;
+
+    if cli.create_config {
+        // this is a meh way to do this
+        let config_new = AppConfig {
+            open_ai_api_key: String::from("EMPTY_TOKEN"),
+            mqtt: None,
+        };
+        config_new.save_user_config()?;
+        return Ok(());
+    }
+
+    if cli.copy_local_config {
+        // this is a meh way to do this
+        let local_config = AppConfig::load_dev_config()?;
+        local_config.save_user_config()?;
+        return Ok(());
+    }
+
+    let config = AppConfig::load_user_config()?;
+
     let client = Client::new().with_api_key(&config.open_ai_api_key);
 
     let mqtt_client = start_mqtt_service(&config.mqtt.context("mqtt config missing")?)?;
 
-    let mut chat_manager =
-        chat_manager::ChatHistory::new("You are Joi. The cheerful helpful AI assistant.")?;
+    let system_messages = generate_system_instructions();
+
+    let mut chat_manager = chat_manager::ChatHistory::new(&system_messages["joi"])?;
+
+    let term = Term::stdout();
 
     loop {
         let (_temp_dir, audio_path) =
             chatty::audio::record_audio_with_cli(cli.jack, cli.device.clone())?;
 
-        println!("Transcribing");
+        term.write_line("Transcribing\n")?;
 
         let request = CreateTranscriptionRequestArgs::default()
             .file(audio_path)
-            .model("whisper-1")
+            .model(TRANSCRIBE_MODEL)
             .build()?;
 
         let response = client.audio().transcribe(request).await?;
         let user_question = response.text;
 
-        println!("User:\n\n{}", user_question);
+        term.write_line(&format!("{QUESTION_MARK_EMOJI} Question:\n{user_question}"))?;
 
-        let response = chat_manager.next_message(&user_question, &client).await?;
+        term.write_line(&format!("\n{ROBOT_EMOJI} ChatGPT:\n"))?;
 
-        println!("ChatGPT:\n\n{}", response);
+        let response = if cli.disable_streaming {
+            let response = chat_manager.next_message(&user_question, &client).await?;
+            term.write_line(&response)?;
+            term.write_line("")?;
+            response
+        } else {
+            let response = chat_manager
+                .next_message_stream_stdout(&user_question, &client, &term)
+                .await?;
+            response
+        };
 
-        mqtt_client
-            .publish("home_speak/say/cheerful", QoS::AtMostOnce, false, response)
-            .await?;
+        if !cli.mute {
+            mqtt_client
+                .publish("home_speak/say/cheerful", QoS::AtMostOnce, false, response)
+                .await?;
+        }
+
+        if !cli.no_save {
+            chat_manager.save_to_file()?;
+        }
 
         wait_for_enter()?;
     }
